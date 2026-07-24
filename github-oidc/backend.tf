5083f2152bd9594b4c60273e1a967b51060995ae
zain <zainazhar079@gmail.com>
2026-07-24 11:36:08 +0100

    Fix OIDC trust policy (sub wildcard, TagSession, drop thumbprint) and separate github-oidc state key

--- github-oidc/backend.tf
+++ github-oidc/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "zain-tfstate-vpc-ec2-webapp-2026" # bootstrap output: state_bucket_name
-    key            = "vpc-ec2-webapp/terraform.tfstate" # path *within* the bucket -- lets one bucket hold state for many projects
+    key            = "github-oidc/terraform.tfstate" # path *within* the bucket -- lets one bucket hold state for many projects
     region         = "eu-west-2"                        # bootstrap output: aws_region
     dynamodb_table = "terraform-locks"                  # bootstrap output: dynamodb_table_name
     encrypt        = true
